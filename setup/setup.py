294df972a72a0165568b33de37371cbb45893644
Dino Gržinić <dino.grzinic@skole.hr>
2021-02-04 20:09:20 +0100

    Add files via upload

--- setup/setup.py
+++ setup/setup.py
@@ -1,9 +1,8 @@
 import sys
 import subprocess
 import pathlib
-print(str(pathlib.Path().absolute())+'\setup')
 
 l=['requests','pyttsx3','wikipedia','ecapture','wolframalpha',
-'SpeechRecognition','winshell','pywin32']
+'SpeechRecognition','winshell','pywin32','youtube-search-python']
 for i in range (len(l)):
     subprocess.check_call([sys.executable, "-m", "pip", "install", l[i]])
